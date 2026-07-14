1fa40edeacf209f4b09f789eb50c856c36b43025
Sai Sathya Jain <saisathyajain@sssihl.edu.in>
2026-07-14 15:43:56 +0530

    UPDATE : removed the uneccessary team drop down box from outward form

--- client/src/components/TeamPortal/TeamPortal.jsx
+++ client/src/components/TeamPortal/TeamPortal.jsx
@@ -960,14 +960,8 @@ function TeamPortal() {
                                 <form id="outward-form" onSubmit={handleSubmit}>
                                     <div className="grid-2">
                                         <div className="form-group">
-                                            <label className="form-label">Your Team *</label>
-                                            <select name="createdByTeam" className="form-select" value={formData.createdByTeam} onChange={handleChange} required>
-                                                <option value="">Select Team...</option>
-                                                <option value="UPAS">UPAS Team</option>
-                                                <option value="PPAS">PPAS Team</option>
-                                                <option value="UPAS/PPAS">UPAS/PPAS Team</option>
-                                                <option value="DPAS">DPAS Team</option>
-                                            </select>
+                                            <label className="form-label">Your Team</label>
+                                            <input type="text" className="form-input tp-outward-no-preview" value={`${selectedTeam} Team`} readOnly disabled/>
                                         </div>
                                         <div className="form-group">
                                             <label className="form-label">Outward No.</label>
